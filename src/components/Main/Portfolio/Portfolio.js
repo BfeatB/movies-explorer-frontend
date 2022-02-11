27f75ea3aecd43458350262cc1381fa55f958490
Valentina Karpova <bfeatb@mail.ru>
2022-02-11 15:06:42 +0000

    feat: add Portfolio component

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -1,18 +1,22 @@
 import React from "react";
+import arrow from "../../../images/arrow.svg"
 
 function Portfolio(props) {
   return (
     <section className="portfolio">
-      <h2 className="portfolio__title"></h2>
+      <h2 className="portfolio__title">Портфолио</h2>
       <ul className="portfolio__list">
         <li className="portfolio__item">
-          <a className="portfolio__link" href="#"></a>
+          <a className="portfolio__link" href="#">Статичный сайт</a>
+          <img className="portfolio__arrow" src={arrow} />
         </li>
-        <li className="portfolio__item" href="#">
-          <a className="portfolio__link"></a>
+        <li className="portfolio__item">
+          <a className="portfolio__link"href="#">Адаптивный сайт</a>
+          <img className="portfolio__arrow" src={arrow} />
         </li>
-        <li className="portfolio__item" href="#">
-          <a className="portfolio__link"></a>
+        <li className="portfolio__item">
+          <a className="portfolio__link"href="#">Одностраничное приложение</a>
+          <img className="portfolio__arrow" src={arrow} />
         </li>
       </ul>
     </section>
