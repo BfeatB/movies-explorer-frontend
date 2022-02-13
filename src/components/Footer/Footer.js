e132ee753bb2a199d8412ad16e0b3cdc755c6e10
Valentina Karpova <bfeatb@mail.ru>
2022-02-13 11:31:18 +0000

    feat: add Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,20 +4,22 @@ function Footer(props) {
   return (
     <footer className="footer">
       <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
-      <nav className="footer__nav">
-        <ul className="footer__list">
-          <li className="footer__item">
-            <a className="footer__link" href="#">Яндекс.Практикум</a>
-          </li>
-          <li className="footer__item">
-            <a className="footer__link" href="#">Github</a>
-          </li>
-          <li className="footer__item">
-            <a className="footer__link" href="#">Facebook</a>
-          </li>
-        </ul>
-      </nav>
-      <p className="footer__date">@2022</p>
+      <div className="footer__container">
+        <p className="footer__date">@2022</p>
+        <nav className="footer__nav">
+          <ul className="footer__list">
+            <li className="footer__item">
+              <a className="footer__link" href="#">Яндекс.Практикум</a>
+            </li>
+            <li className="footer__item">
+              <a className="footer__link" href="#">Github</a>
+            </li>
+            <li className="footer__item">
+              <a className="footer__link" href="#">Facebook</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </footer>
   );
 }
