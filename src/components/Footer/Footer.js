c9a9ab8785ad98612044547ccb5c06b6fb2b8990
Valentina Karpova <bfeatb@mail.ru>
2022-02-13 12:42:58 +0000

    feat: add small screens Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ function Footer(props) {
       <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
       <div className="footer__container">
         <p className="footer__date">@2022</p>
-        <nav className="footer__nav">
+        <nav>
           <ul className="footer__list">
             <li className="footer__item">
               <a className="footer__link" href="#">Яндекс.Практикум</a>
