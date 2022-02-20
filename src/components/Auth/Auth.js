88b29effd9cf2f0558413901f5eecb2da4eb63ca
Valentina Karpova <bfeatb@mail.ru>
2022-02-20 14:54:24 +0000

    feat: input error

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -9,12 +9,12 @@ function Auth(props) {
         <a className="auth__logo-link link" href="/"><img src={logo} alt="logo"></img></a>
         <h1 className="auth__title">{props.isSignedIn ? "Рады видеть!" : "Добро пожаловать!"}</h1>
         <form className="auth__form">
-            {!props.isSignedIn && <InputRow label="Имя" placeholder="Введите имя" type="text" />}
-            <InputRow label="E-mail" placeholder="Введите e-mail" type="email" />
-            <InputRow label="Пароль" placeholder="Введите пароль" type="password" />
-            <button className="auth__button button">{!props.isSignedIn ? "Зарегистрироваться" : "Войти"}</button>
-            {!props.isSignedIn && <Footer prefixText="Уже зарегистрированы?" linkText="Войти" linkHref="/signin" />}
-            {props.isSignedIn && <Footer prefixText="Еще не зарегистрированы?" linkText="Регистрация" linkHref="/signup" />}
+          {!props.isSignedIn && <InputRow label="Имя" placeholder="Введите имя" type="text" />}
+          <InputRow label="E-mail" placeholder="Введите e-mail" type="email" />
+          <InputRow label="Пароль" placeholder="Введите пароль" value="wrongpassword" type="password" error="Что-то пошло не так..." />
+          <button className="auth__button button">{!props.isSignedIn ? "Зарегистрироваться" : "Войти"}</button>
+          {!props.isSignedIn && <Footer prefixText="Уже зарегистрированы?" linkText="Войти" linkHref="/signin" />}
+          {props.isSignedIn && <Footer prefixText="Еще не зарегистрированы?" linkText="Регистрация" linkHref="/signup" />}
         </form>
       </div>
     </main>
@@ -25,7 +25,8 @@ function InputRow(props) {
   return (
     <>
       <label className="auth__label">{props.label}</label>
-      <input className="auth__field" type={props.type} placeholder={props.placeholder} />
+      <input className={`auth__field ${props.error ? "auth__field_error" : ""}`} type={props.type} placeholder={props.placeholder} value={props.value} />
+      {props.error && <span className="auth__error">{props.error}</span>}
     </>
   )
 }
