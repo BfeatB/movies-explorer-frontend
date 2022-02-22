c06e3164f8ed005a43eec4b580fb7162f3a1a331
Valentina Karpova <bfeatb@mail.ru>
2022-02-22 16:19:22 +0000

    fix: add requited to a search field

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -5,14 +5,14 @@ function SearchForm(props) {
     <div className="search">
       <form className="search__form">
         <div className="search__wrapper">
-          <input className="search__field" type="search" placeholder="Фильм"></input>
+          <input className="search__field" type="search" placeholder="Фильм" required></input>
           <button className="search__button button" type="submit">Найти</button>
         </div>
-          <label className="search__label">Короткометражки</label>
-          <div class="search__toggle-wrapper">
+        <label className="search__label">Короткометражки</label>
+        <div class="search__toggle-wrapper">
           <input class="search__checkbox" type="checkbox" name="toggle" id="toggle"></input>
-           <label for="toggle"></label>
-          </div>
+          <label for="toggle"></label>
+        </div>
       </form>
     </div>
   );
